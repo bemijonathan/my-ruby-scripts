ff07e7988a35cdaece7829808315bbfb52f9577c
bemijonathan <bemijonathan@gmail.com>
2020-02-26 12:39:17 +0100

    done linear regression

--- rails-ml/index.rb
+++ rails-ml/index.rb
@@ -1,25 +1,26 @@
 require 'daru'
 require 'nyaplot'
-require 'rumale'
+require 'ruby_linear_regression'
 
-data = Daru::DataFrame.from_csv("./scores.csv")
+  data = Daru::DataFrame.from_csv("new.csv")
 
-data.plot(type: :scatter, x: 'absences', y: 'failures') do |p, d|
-  p.xrange [-10, 10]
-  p.yrange [-10, 10]
-  p.export_html("line.html")
-  # `open line.html`
-end
+  data.plot(type: :scatter, x: 'GROSS SQUARE FEET', y: 'LAND SQUARE FEET') do |p, d|
+    p.export_html("line.html")
+    # `open line.html`
+  end
 
-x = data["absences"]
-
-y = data["absences"]
-
-
-
-  estimator = Rumale::LinearModel::LinearRegression.new()
-  estimator.fit(x.to_a, y.to_a)
-  # results = estimator.predict(5)
-  # puts results
-
-  # puts x.to_a
+  x = data['GROSS SQUARE FEET']
+  y = data['SALE PRICE']
+  d = []
+  c = x.to_a
+  c.each { |s| d.push([s])}
+  x = d
+  puts d.inspect
+  puts x.to_a.inspect
+  puts y.to_a.inspect
+  linear_regression = RubyLinearRegression.new
+  linear_regression.load_training_data(x, y)
+  linear_regression.train_normal_equation
+  prediction_data = [3000]
+  predicted_price = linear_regression.predict(prediction_data)
+  puts "Predicted price for a 1500 sq feet property: #{predicted_price.round}$"
